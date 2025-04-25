4c0354eb176ce489af2defdecfd021c5e8ae6309
raf <raf@notashelf.dev>
2025-04-25 12:39:36 +0000

    nix: init package (#8)
    * nix: init package
    
    * nix: add yt-dlp to runtime

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
+
+  outputs = {
+    nixpkgs,
+    self,
+    ...
+  }: let
+    inherit (nixpkgs) legacyPackages lib;
+
+    # Compose for multiple systems. Please do not replace this with nix-systems as it is
+    # not guaranteed to build on platforms we haven't tested on.
+    systems = ["x86_64-linux"];
+    forEachSystem = lib.genAttrs systems;
+    pkgsForEach = legacyPackages;
+  in {
+    # Provide a formatter for `nix fmt`
+    formatter = forEachSystem (system: nixpkgs.legacyPackages.${system}.alejandra);
+
+    packages = forEachSystem (system: let
+      pkgs = pkgsForEach.${system};
+    in {
+      default = self.packages.${system}.vermillion;
+      vermillion = pkgs.callPackage ./nix/package.nix {inherit self;};
+    });
+
+    devShells = forEachSystem (system: let
+      pkgs = pkgsForEach.${system};
+    in {
+      default = self.devShells.${system}.vermillion;
+      vermillion = pkgs.mkShellNoCC {
+        name = "vermillion-dev";
+        packages = with pkgs; [
+          # Eslint_d
+          nodejs-slim
+          pnpm
+        ];
+      };
+    });
+  };
+}
---
+++ nix/package.nix
@@ -0,0 +1,113 @@
+{
+  self,
+  lib,
+  stdenvNoCC,
+  nodejs,
+  pnpm_10,
+  makeWrapper,
+  electron,
+  xdg-utils,
+  yt-dlp,
+  ...
+}: let
+  fs = lib.fileset;
+in
+  stdenvNoCC.mkDerivation (finalAttrs: {
+    pname = "vermillion";
+    version =
+      if (self ? rev)
+      then (builtins.substring 0 7 self.rev)
+      else "dirty";
+
+    src = let
+      sp = ../.;
+    in
+      fs.toSource {
+        root = sp;
+
+        # Filter everything outside of what's specified here. Configuration files
+        # are good to include, but linter/formatter configs are not necessary.
+        fileset = fs.intersection (fs.fromSource (lib.sources.cleanSource sp)) (
+          fs.unions [
+            ../build
+            ../resources
+            ../src
+            ../package.json
+            ../pnpm-lock.yaml
+            ../svelte.config.mjs
+            ../electron-builder.yml
+            ../electron.vite.config.ts
+            ../tsconfig.json
+            ../tsconfig.node.json
+            ../tsconfig.web.json
+          ]
+        );
+      };
+
+    pnpmDeps = pnpm_10.fetchDeps {
+      inherit (finalAttrs) pname src;
+      hash = "sha256-al2WTriEbOfpObi69gQ2ykwanQ5t6EvrryyUVbdSb44=";
+    };
+
+    nativeBuildInputs = [
+      makeWrapper
+      pnpm_10.configHook
+      nodejs
+    ];
+
+    # npm install will error when electron tries to download its binary
+    # we don't need it anyways since we wrap the program with our nixpkgs electron
+    env.ELECTRON_SKIP_BINARY_DOWNLOAD = "1";
+
+    buildPhase = ''
+      runHook preBuild
+
+      cp -r ${electron.dist} electron-dist
+      chmod -R u+w electron-dist # for possible Darwin support in the future
+
+      pnpm run build
+      pnpm exec electron-builder \
+        --dir \
+        -c.electronDist=electron-dist \
+        -c.electronVersion="${electron.version}" \
+
+      runHook postBuild
+    '';
+
+    # This is, by far, the most stupid way of packaging an application. Not my fault though
+    # because Electron is stupid. The method employed here is easier (and cheaper) than
+    # building the appimage, then making Nix aware of it, AND then unpacking it. You will
+    # *never* catch me unpacking an appimage again.
+    # - raf
+    installPhase = let
+      binPath = lib.makeBinPath [
+        # For Electron & file pickers
+        xdg-utils
+
+        # For YT-Music integration
+        # https://github.com/vaxerski/Vermilion/blob/main/docs/YTM.md
+        yt-dlp
+      ];
+    in ''
+      runHook preInstall
+
+      mkdir -p $out/share/vermillion
+      cp -r dist/*-unpacked/{locales,resources{,.pak}} $out/share/vermillion
+
+      makeWrapper '${lib.getExe electron}' "$out/bin/vermillion" \
+        --inherit-argv0 \
+        --add-flags $out/share/vermillion/resources/app.asar \
+        --suffix PATH : "${binPath}" \
+        --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform-hint=auto --enable-features=WaylandWindowDecorations --enable-wayland-ime=true}}"
+
+      runHook postInstall
+    '';
+
+    meta = {
+      description = "Clean, minimal and simple music player for MPD and Tidal";
+      homepage = "https://github.com/vaxerski/vermilion";
+      license = lib.licenses.bsd3;
+      maintainers = [lib.maintainers.NotAShelf];
+      platforms = lib.platforms.linux;
+    };
+  })
